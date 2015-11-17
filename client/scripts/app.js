cee5bf1622adad6da6a1e7692f5361a65752af06
MKS-students <mks.students@hackreactor.com>
2015-11-17 16:46:05 -0600

    Added POST functionality

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,21 +1,17 @@
 
-var message = {
-  username: 'Mel Brooks',
-  text: 'It\'s good to be the king',
-  roomname: 'lobby'
-};
 
 var charsNotAllowed = ['&', '<', '>', '"', "'", "/"];
 
 var app = {
    server: 'https://api.parse.com/1/classes/chatterbox',
    init: function(){
+
      app.fetch();
    },
    send: function(message){
+
      $.ajax({
   // This is the url you should use to communicate with the parse API server.
-
       url: app.server,
       type: 'POST',
       data: JSON.stringify(message),
@@ -24,10 +20,12 @@ var app = {
         console.log('chatterbox: Message sent');
       },
       error: function (data) {
+        console.log(data);
         // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
         console.error('chatterbox: Failed to send message');
       }
-    });
+    })
+     app.fetch();
    },
 
    fetch: function(){
@@ -37,17 +35,18 @@ var app = {
         type: 'GET',
         contentType: 'application/json',
         success: function (data) {
-        //   _.each(data.results, function(item){
-        //     $('#messages').append('<li>'+item.text+'</li>');
-        // });
-
-        _.each(data.results, function(dataItem){
-          //console.log(dataItem.text);
-          screwYouDan(dataItem.text);
-
-            $('#main').append("<div class=chat>" +dataItem.text+ "</div>")
-
-        })
+          var resultsArray = data.results;
+          console.log("resultsArray:", resultsArray);
+          resultsArray.forEach(function(messageObj, index){
+            if(messageObj.text){
+              if(messageObj.text.indexOf('<') > -1){
+                resultsArray.splice(index, 1);
+              } else {
+                divString = "<div class=chat username>" + messageObj.username + "<div class=chat>" +messageObj.text+ "</div>"+"</div>"
+                $('#main').append(divString);
+              }
+            }
+          })
         },
         error: function (data) {
           // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
@@ -60,18 +59,25 @@ var app = {
    clearMessages: function(){
      $('#main').children().html('');
    },
-   addMessage: function(message){
-     app.send(message);
+   addMessage: function(){
+    // e.preventdefault();
+     if(window.document.getElementById('Message').value !== "") {
+       var username = window.document.URL.split('=')[1],
+           text = window.document.getElementById('Message').value;
+       var message = app.makeMessage(username, text, 'lobby');
+     }
+       this.send(message);
+       // app.fetch();
    },
-   addRoom: function(){}
+   addRoom: function(){},
+   makeMessage: function(username, text, roomname){
+      var myObj = {}
+      myObj.username = username || null, //
+      myObj.text = text || null, //
+      myObj.roomname = roomname || null
+      return myObj;
+   }
 
 }
 app.init();
 
-function screwYouDan(str){
-  if(str.match(/<script>/)){
-    str = "I'm an idiot"
-  } else {
-    return str;
-  }
-}

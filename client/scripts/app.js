a10e376cb68ef7c619dedce0ef91aab1c8b9ed8e
MKS-students <mks.students@hackreactor.com>
2015-11-16 19:54:36 -0600

    Did some stuff, got owned by the green raccoon

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -5,12 +5,14 @@ var message = {
   roomname: 'lobby'
 };
 
+var charsNotAllowed = ['&', '<', '>', '"', "'", "/"];
+
 var app = {
    server: 'https://api.parse.com/1/classes/chatterbox',
    init: function(){
      app.fetch();
    },
-   send: function(){
+   send: function(message){
      $.ajax({
   // This is the url you should use to communicate with the parse API server.
 
@@ -35,21 +37,41 @@ var app = {
         type: 'GET',
         contentType: 'application/json',
         success: function (data) {
-          _.each(data, function(message){
-            $('h1').append(message.message);
-          })
+        //   _.each(data.results, function(item){
+        //     $('#messages').append('<li>'+item.text+'</li>');
+        // });
+
+        _.each(data.results, function(dataItem){
+          //console.log(dataItem.text);
+          screwYouDan(dataItem.text);
+
+            $('#main').append("<div class=chat>" +dataItem.text+ "</div>")
+
+        })
         },
         error: function (data) {
           // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
           // console.error('chatterbox: Failed to send message');
           console.log("Hey from the ajax fetch error", data)
         }
-      });
+      })
    },
 
-   clearMessages: function(){},
-   addMessage: function(){},
+   clearMessages: function(){
+     $('#main').children().html('');
+   },
+   addMessage: function(message){
+     app.send(message);
+   },
    addRoom: function(){}
 
 }
 app.init();
+
+function screwYouDan(str){
+  if(str.match(/<script>/)){
+    str = "I'm an idiot"
+  } else {
+    return str;
+  }
+}
